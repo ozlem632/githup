30603cc93fae1509a9dfee8383c34548b6cbfc1a
Ozlem Asimoglu <Ozlemasimoglu@gmail.com>
2023-02-12 12:07:14 +0100

    version3

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -2,7 +2,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("merhaba git");
-
+        System.out.println("pushlama");
 
     }
 }
